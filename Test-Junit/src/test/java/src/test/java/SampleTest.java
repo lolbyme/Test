5ac70032e060ad65dd2d108ef257f6cdfd45a22d
lolbyme <32265064+lolbyme@users.noreply.github.com>
2017-09-28 22:57:58 +0200

    ya
    ya

---
+++ Test-Junit/src/test/java/src/test/java/SampleTest.java
@@ -0,0 +1,21 @@
+package src.test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void SimpleTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue((a+b)==3);
+	}
+	@Test
+	public void SimpleTest1() {
+		int c = 2;
+		int d = 2;
+		assertTrue((c+d)==4);
+	}
+}
