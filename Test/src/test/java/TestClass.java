afb962aacd84dc4877279d6d5cc1db196a0bb2ea
lolbyme <32265064+lolbyme@users.noreply.github.com>
2017-09-29 12:19:06 +0200

    a
    a

--- Test/src/test/java/TestClass.java
+++ Test/src/test/java/TestClass.java
@@ -1,10 +1,8 @@
-package src.test.java;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class SampleTest {
+public class TestClass {
 
 	@Test
 	public void SimpleTest() {
